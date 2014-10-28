5c98b0332d14a56994da590f492e7978e9c8c760
Jayme Woogerd <jayme.woogerd@tufts.edu>
2014-10-28 12:43:53 -0400

    Rough first pass of data structures for first example

--- Language/Pollstr_syntax.hs
+++ Language/Pollstr_syntax.hs
@@ -1,5 +1,18 @@
+module Language.Pollstr_syntax where
 
-Data Question   = Question String deriving Show
-Data Response a = Response [a] deriving Show
+type ID         = String
 
-Data Item = Item (Question, Response) deriving Show
+data Survey     = Survey (ID, [Decl], [SurveyItem]) deriving Show
+
+data Question   = Question String deriving Show
+
+data Response   = Response [String] deriving Show
+
+data Decl       = QuestDelc (ID, Question) 
+                | RespDecl (ID, Response) deriving Show
+
+data SurveyItem = SurveyItem (ID, Question, Response) deriving Show
+
+declarations = [RespDecl("howFrequent", Response ["Never", "Sometimes", "Often", "Always"])]
+items = [SurveyItem("teeth", Question "How often do you brush your teeth?", Response ["Never", "Sometimes", "Often", "Always"] )]
+test = Survey("mySurvey", declarations, items)
