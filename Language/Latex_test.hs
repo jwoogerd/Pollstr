8f63cb27052466c60287124ab85532b4ffb01cb0
Jayme Woogerd <jayme.woogerd@tufts.edu>
2014-11-18 19:51:25 -0500

    Generate the ugliest of LaTeX files

--- Language/Latex_test.hs
+++ Language/Latex_test.hs
@@ -2,7 +2,8 @@
 
 module Language.Latex_test where
 
-import Text.LaTeX
+import Text.LaTeX hiding (item)
+import qualified Text.LaTeX as LT (item)
 
 import Examples.SimpleSurvey
 
@@ -22,19 +23,38 @@ preamble :: Monad m => LaTeXT_ m
 preamble = documentclass [] article
 
 surveyL :: Monad m => Survey -> LaTeXT_ m
-surveyL (Survey (id, decls, items, sections)) = 
+surveyL (Survey id decls items sections) = 
     preamble
     <> title (fromString id)
-    <> itemsL decls items
+    <> document (itemsL decls items)
 
 itemsL :: Monad m => [Decl] -> [Item] -> LaTeXT_ m
 itemsL decls items = 
-    map mkItem items
-    where mkItem
+    let mkItem (Item id quest resp skp) = 
+            (questionL quest decls)
+            <> responseL resp decls
+            <> skipL skp decls
+    in mconcat $ map mkItem items
 
-itemsL :: Monad m => Question -> LaTeXT_ m
-questionL Question q = fromString q
---questionL Qvar    qv = case qv `elem` 
+questionL :: Monad m => Question -> [Decl] -> LaTeXT_ m
+questionL (Question q) _  = fromString q
+questionL (Qvar qv) decls = case lookupQ decls of Just q  -> questionL q []
+                                                  Nothing -> error "Not found"
+    where lookupQ []                    = Nothing
+          lookupQ (QuestDecl id q:rest) = if qv == id then Just q else lookupQ rest
+          lookupQ (_:rest)              = lookupQ rest
+
+responseL :: Monad m => Response -> [Decl] -> LaTeXT_ m
+responseL (Response rs) _  = mconcat $ fmap fromString rs
+responseL (Rvar rv) decls = case lookup decls of Just r  -> responseL r []
+                                                 Nothing -> error "Not found"
+    where lookup []                   = Nothing                                            
+          lookup (RespDecl id r:rest) = if rv == id then Just r else lookup rest
+          lookup (_:rest)             = lookup rest
+
+skipL :: Monad m => Skip -> [Decl] -> LaTeXT_ m
+skipL None _ = fromString ""
+skipL (Skip id r) decls = fromString "Not implemented"
 
 sectionsL :: Monad m => [Section] -> LaTeXT_ m
 sectionsL = undefined
